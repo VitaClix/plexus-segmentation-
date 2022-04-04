54142840bae88a73e5876698f85800d8f3d2a4d1
VitaClix <vitaclix@proton.me>
2022-04-04 20:06:55 +0000

    Forgot to add

--- run_gmm_chp_segmentation.py
+++ run_gmm_chp_segmentation.py
@@ -20,3 +20,19 @@ def run_cmd(cmd):
     out,err = unix_cmd(cmd)
     print out
     show_error(err)
+    return out,err
+    
+def save_segmentation(clf,out_name):
+    new_img = np.zeros((256,256,256))
+
+    #choroid_ind = np.where(np.mean(clf.means_)==np.max(clf.means_))
+
+    if np.mean(mask_T1_vals[clf.predict(X)==1]) > np.mean(mask_T1_vals[clf.predict(X)==0]): 
+    
+        choroid_ind = np.where(clf.predict(X)==1)[0]
+    else: 
+
+        choroid_ind = np.where(clf.predict(X)==0)[0]
+
+    choroid_coords = (mask_indices[0][choroid_ind], mask_indices[1][choroid_ind], mask_indices[2][choroid_ind])
+    new_img[choroid_coords] = 1
